d3fd1f6e74c83958effdafa5b8322ff9abdf2938
brainhunter12221 <eurochamion2016portugal@gmail.com>
2024-01-10 13:05:42 +0200

    Initial commit v0.2 Set up classes Book, Library and LibraryManagement with core fields and methods

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,4 +1,44 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Library {
+    private ArrayList<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public Book findBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public void deleteBookByISBN(String isbn) {
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getIsbn().equals(isbn)) {
+                iterator.remove();
+                System.out.println("Book with ISBN " + isbn + " deleted.");
+                return;
+            }
+        }
+        System.out.println("Book with ISBN " + isbn + " not found.");
+    }
 }
--- src/main/java/org/example/LibraryManagement.java
+++ src/main/java/org/example/LibraryManagement.java
@@ -1,4 +1,32 @@
 package org.example;
 
 public class LibraryManagement {
+    public static void main(String[] args) {
+        Library library = new Library();
+
+        // Додавання книг у бібліотеку
+        library.addBook(new Book("Назва1", "Автор1", "ISBN111", 2000));
+        library.addBook(new Book("Назва2", "Автор2", "ISBN222", 2010));
+
+        // Показ усіх книг в бібліотеці
+        System.out.println("Усі книги в бібліотеці:");
+        library.displayBooks();
+
+        // Пошук книги за назвою
+        String searchTitle = "Назва1";
+        Book foundBook = library.findBookByTitle(searchTitle);
+        if (foundBook != null) {
+            System.out.println("Знайдено книгу за назвою '" + searchTitle + "': " + foundBook);
+        } else {
+            System.out.println("Книга за назвою '" + searchTitle + "' не знайдена.");
+        }
+
+        // Видалення книги за ISBN
+        String isbnToDelete = "ISBN111";
+        library.deleteBookByISBN(isbnToDelete);
+
+        // Повторний показ усіх книг після видалення
+        System.out.println("Усі книги в бібліотеці після видалення:");
+        library.displayBooks();
+    }
 }
